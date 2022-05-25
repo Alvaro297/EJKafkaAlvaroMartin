dd96d3a6488a04885263589193efa67da4a4788a
Alvaro297 <alvaromartinfalagan29@gmail.com>
2022-05-25 09:38:18 +0200

    Kafka Alvaro Martin

---
+++ src/main/java/com/example/KafkaAlvaroMartin/KafkaAlvaroMartinApplication.java
@@ -0,0 +1,13 @@
+package com.example.KafkaAlvaroMartin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAlvaroMartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAlvaroMartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KafkaAlvaroMartin/KafkaAlvaroMartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KafkaAlvaroMartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAlvaroMartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
